8723b2ee5d10ce0b0edd51ff58d2b44a76dda865
Kramerbaf <nds.kram@gmail.com>
2018-12-03 19:30:02 +0100

    update

--- src/section.vue
+++ src/section.vue
@@ -1,17 +1,13 @@
 <template>
-
     <div class="feature_C section-side-padding">
-
         <!-- wwManager:start -->
         <wwSectionEditMenu v-bind:sectionCtrl="sectionCtrl"></wwSectionEditMenu>
         <!-- wwManager:end -->
-
-        <wwObject class="background" v-bind:ww-object="section.data.background" ww-category="background">
-        </wwObject>
+        <wwObject class="background" v-bind:ww-object="section.data.background" ww-category="background"></wwObject>
 
         <!--TOP WWOBJS-->
         <div class="top-ww-objs">
-            <wwLayoutColumn tag='div' ww-default="ww-image" :ww-list="section.data.topWwObjs" class="top-ww-obj" @ww-add="add(section.data.topWwObjs, $event)" @ww-remove="remove(section.data.topWwObjs, $event)">
+            <wwLayoutColumn tag="div" ww-default="ww-image" :ww-list="section.data.topWwObjs" class="top-ww-obj" @ww-add="add(section.data.topWwObjs, $event)" @ww-remove="remove(section.data.topWwObjs, $event)">
                 <wwObject v-for="topWwObj in section.data.topWwObjs" :key="topWwObj.uniqueId" v-bind:ww-object="topWwObj"></wwObject>
             </wwLayoutColumn>
         </div>
@@ -23,7 +19,7 @@
                     <div>
                         <wwObject class="background" v-bind:ww-object="thumbnail.background" ww-category="background" ww-default-object-type="ww-color"></wwObject>
 
-                        <wwLayoutColumn tag='div' ww-default="ww-image" :ww-list="thumbnail.contents" class="content" @ww-add="add(thumbnail.contents, $event)" @ww-remove="remove(thumbnail.contents, $event)">
+                        <wwLayoutColumn tag="div" ww-default="ww-image" :ww-list="thumbnail.contents" class="content" @ww-add="add(thumbnail.contents, $event)" @ww-remove="remove(thumbnail.contents, $event)">
                             <wwObject v-for="content in thumbnail.contents" :key="content.uniqueId" v-bind:ww-object="content"></wwObject>
                         </wwLayoutColumn>
                     </div>
@@ -33,13 +29,11 @@
 
         <!--BOTTOM WWOBJS-->
         <div class="bottom-ww-objs">
-            <wwLayoutColumn tag='div' ww-default="ww-image" :ww-list="section.data.bottomWwObjs" class="top-ww-obj" @ww-add="add(section.data.bottomWwObjs, $event)" @ww-remove="remove(section.data.bottomWwObjs, $event)">
+            <wwLayoutColumn tag="div" ww-default="ww-image" :ww-list="section.data.bottomWwObjs" class="top-ww-obj" @ww-add="add(section.data.bottomWwObjs, $event)" @ww-remove="remove(section.data.bottomWwObjs, $event)">
                 <wwObject v-for="bottomWwObj in section.data.bottomWwObjs" :key="bottomWwObj.uniqueId" v-bind:ww-object="bottomWwObj"></wwObject>
             </wwLayoutColumn>
         </div>
-
     </div>
-
 </template>
 
 <script>
@@ -51,30 +45,14 @@ export default {
     },
     data() {
         return {
+            thumbnailsPerLine: 4,
+            maxThumbnailsPerLine: 4,
+            columnWidth: { 'width': "calc(25% - 30px)" }
         }
     },
     computed: {
         section() {
             return this.sectionCtrl.get();
-        },
-        columnWidth() {
-            switch (this.section.data.thumbnails.length) {
-                case 4:
-                    return { 'width': "calc(25% - 30px)" };
-                    break;
-                case 3:
-                    return { 'width': "calc(33.3333% - 30px)" };
-                    break;
-                case 2:
-                    return { 'width': "calc(50% - 30px)" };
-                    break;
-                case 1:
-                    return { 'width': "calc(100% - 30px)" };
-                    break;
-
-                default:
-                    break;
-            }
         }
     },
     methods: {
@@ -124,7 +102,8 @@ export default {
             }
         },
         init() {
-            window.addEventListener("resize", this.updateThumbnailContainer);
+            this.setThumbnailsPerLine();
+            window.addEventListener("resize", this.setThumbnailsPerLine);
         },
         add(list, options) {
             list.splice(options.index, 0, options.wwObject);
@@ -135,6 +114,36 @@ export default {
             list.splice(options.index, 1);
 
             this.sectionCtrl.update(this.section);
+        },
+        setThumbnailsPerLine() {
+            let width = window.innerWidth;
+            if (width < 576) {
+                this.maxThumbnailsPerLine = 1;
+            }
+            else if (width < 992) {
+                this.maxThumbnailsPerLine = 2;
+            }
+            else if (width < 1200) {
+                this.maxThumbnailsPerLine = 3;
+            }
+            else {
+                this.maxThumbnailsPerLine = 4;
+            }
+
+            switch (Math.min(this.thumbnailsPerLine, this.maxThumbnailsPerLine)) {
+                case 1:
+                    this.columnWidth = { 'width': "calc(100% - 30px)" };
+                    break;
+                case 2:
+                    this.columnWidth = { 'width': "calc(50% - 30px)" };
+                    break;
+                case 3:
+                    this.columnWidth = { 'width': "calc(33.3333% - 30px)" };
+                    break;
+                default:
+                    this.columnWidth = { 'width': "calc(25% - 30px)" };
+                    break;
+            }
         }
     },
     created() {
@@ -144,7 +153,7 @@ export default {
         this.init();
     },
     beforeDestroyed() {
-        window.removeEventListener("resize", this.updateThumbnailContainer);
+        window.removeEventListener("resize", this.setThumbnailsPerLine);
     }
 };
 </script>
@@ -152,173 +161,167 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .feature_C {
-  position: relative;
+    position: relative;
 }
 .feature_C .background {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
 }
 
 .feature_C .add-ww-obj-container {
-  width: 100%;
-  text-align: center;
-  margin: 10px 0;
-  position: relative;
+    width: 100%;
+    text-align: center;
+    margin: 10px 0;
+    position: relative;
 }
 
 .feature_C .add-ww-obj-container .add-ww-obj {
-  display: inline-block;
-  height: 40px;
-  width: 40px;
-  line-height: 40px;
-  text-align: center;
-  background-color: white;
-  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.5);
-  border-radius: 3px;
-  cursor: pointer;
+    display: inline-block;
+    height: 40px;
+    width: 40px;
+    line-height: 40px;
+    text-align: center;
+    background-color: white;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.5);
+    border-radius: 3px;
+    cursor: pointer;
 }
 
 .feature_C .remove-ww-obj {
-  position: absolute;
-  top: 0;
-  left: 0;
-  transform: translate(-50%, -50%);
-  height: 30px;
-  width: 30px;
-  background-color: white;
-  color: #92979c;
-  box-shadow: 0px 1px 1px 0px #656565;
-  -moz-box-shadow: 0px 1px 1px 0px #656565;
-  -webkit-box-shadow: 0px 1px 1px 0px #656565;
-  border-radius: 100%;
-  text-align: center;
-  line-height: 30px;
-  cursor: pointer;
-  z-index: 2;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform: translate(-50%, -50%);
+    height: 30px;
+    width: 30px;
+    background-color: white;
+    color: #92979c;
+    box-shadow: 0px 1px 1px 0px #656565;
+    -moz-box-shadow: 0px 1px 1px 0px #656565;
+    -webkit-box-shadow: 0px 1px 1px 0px #656565;
+    border-radius: 100%;
+    text-align: center;
+    line-height: 30px;
+    cursor: pointer;
+    z-index: 2;
 }
 
 .top-ww-objs,
 .bottom-ww-objs {
-  position: relative;
+    position: relative;
 }
 
 .feature_C .top-ww-obj,
 .bottom-ww-obj {
-  position: relative;
+    position: relative;
 }
 
 .feature_C .block .block-img-container {
-  height: 260px;
-  width: 100%;
-  /*overflow: hidden;*/
-  position: relative;
+    height: 260px;
+    width: 100%;
+    /*overflow: hidden;*/
+    position: relative;
 }
 
 .feature_C .add-thumbnail-container {
-  padding: 30px;
-  text-align: center;
+    padding: 30px;
+    text-align: center;
 }
 
 .feature_C .add-thumbnail {
-  display: inline-block;
-  background-color: white;
-  width: 150px;
-  padding: 10px;
-  text-align: center;
-  cursor: pointer;
-  color: #92979c;
-  border-radius: 3px;
-  -moz-border-radius: 3px;
-  -webkit-border-radius: 3px;
-  box-shadow: 0px 1px 1px 0px #656565;
+    display: inline-block;
+    background-color: white;
+    width: 150px;
+    padding: 10px;
+    text-align: center;
+    cursor: pointer;
+    color: #92979c;
+    border-radius: 3px;
+    -moz-border-radius: 3px;
+    -webkit-border-radius: 3px;
+    box-shadow: 0px 1px 1px 0px #656565;
 }
 
 .feature_C .thumbnail-container {
-  margin-right: 15px;
-  position: relative;
-  margin: 30px 15px;
-  background-color: white;
-  min-height: 50px;
-  box-shadow: 0 10px 40px 0 rgba(113, 124, 137, 0.2);
-  padding: 0 45px;
-  border-radius: 7px;
-  -moz-border-radius: 7px;
-  -webkit-border-radius: 7px;
-  -moz-transition: all 0.4s ease-out;
-  -o-transition: all 0.4s ease-out;
-  -ms-transition: all 0.4s ease-out;
-  -webkit-transition: all 0.4s ease-out;
-  transition: all 0.4s ease-out;
+    margin-right: 15px;
+    position: relative;
+    margin: 30px 15px;
+    background-color: white;
+    min-height: 50px;
+    box-shadow: 0 10px 40px 0 rgba(113, 124, 137, 0.2);
+    padding: 0 45px;
+    border-radius: 7px;
+    transition: transform 0.4s ease-out, box-shadow 0.4s ease-out;
 }
 
 .feature_C .thumbnail-container:hover {
-  box-shadow: 0 16px 32px rgba(113, 124, 137, 0.4);
-  transform: translateY(-10px);
+    box-shadow: 0 16px 32px rgba(113, 124, 137, 0.4);
+    transform: translateY(-10px);
 }
 
 .feature_C .thumbnail-container .background {
-  border-radius: 7px;
-  overflow: hidden;
+    border-radius: 7px;
+    overflow: hidden;
 }
 
 .feature_C .remove-thumbnail {
-  position: absolute;
-  top: 0;
-  right: -20px;
-  transform: translate(calc(50% - 15px), -50%);
-  height: 30px;
-  width: 30px;
-  background-color: white;
-  color: #e83154;
-  box-shadow: 0px 1px 1px 0px #656565;
-  -moz-box-shadow: 0px 1px 1px 0px #656565;
-  -webkit-box-shadow: 0px 1px 1px 0px #656565;
-  border-radius: 100%;
-  -moz-border-radius: 100%;
-  -webkit-border-radius: 100%;
-  text-align: center;
-  line-height: 30px;
-  cursor: pointer;
-  z-index: 2;
+    position: absolute;
+    top: 0;
+    right: -20px;
+    transform: translate(calc(50% - 15px), -50%);
+    height: 30px;
+    width: 30px;
+    background-color: white;
+    color: #e83154;
+    box-shadow: 0px 1px 1px 0px #656565;
+    -moz-box-shadow: 0px 1px 1px 0px #656565;
+    -webkit-box-shadow: 0px 1px 1px 0px #656565;
+    border-radius: 100%;
+    -moz-border-radius: 100%;
+    -webkit-border-radius: 100%;
+    text-align: center;
+    line-height: 30px;
+    cursor: pointer;
+    z-index: 2;
 }
 
 .feature_C .thumbnail-container .content {
-  position: relative;
+    position: relative;
 }
 
 .popup-feature_C-column-count .radio-size {
-  height: 30px;
-  width: 250px;
-  margin-top: 30px;
-  display: inline-block;
+    height: 30px;
+    width: 250px;
+    margin-top: 30px;
+    display: inline-block;
 }
 
 .feature_C .card-container-for-two {
-  width: 80%;
-  margin-left: 10%;
+    width: 80%;
+    margin-left: 10%;
 }
 
 .feature_C .card-container-for-three {
-  width: 90%;
-  margin-left: 5%;
+    width: 90%;
+    margin-left: 5%;
 }
 
 .feature_C .card-container-for-four {
-  width: 100%;
-  margin-left: 0%;
+    width: 100%;
+    margin-left: 0%;
 }
 
 .feature_C .container-center {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
 }
 
 @media (min-width: 768px) {
-  /* .feature_C .card-container-for-two {
+    /* .feature_C .card-container-for-two {
         width: 90%;
         margin-left: 5%;
     }
@@ -329,14 +332,14 @@ export default {
 }
 
 @media (min-width: 992px) {
-  /* .feature_C .card-container-for-two {
+    /* .feature_C .card-container-for-two {
         width: 90%;
         margin-left: 5%;
     } */
 }
 
 @media (min-width: 1200px) {
-  /* .feature_C .card-container-for-two {
+    /* .feature_C .card-container-for-two {
         width: 90%;
         margin-left: 5%;
     } */
